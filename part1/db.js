85d552198a7adf9303fe8171bb9badac3c25c2e5
ThanhNguyenPham-Steve <150330770+ThanhNguyenPham-Steve@users.noreply.github.com>
2025-06-20 05:12:31 +0000

    [automatic commit] 2025-06-20 05:12:31 UTC 203.59.201.100

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
   socketPath:
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'newpassword',
   database: 'DogWalkService'
