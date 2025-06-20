b39b4ef64d00fb28b2706f9b68c9aac43ff24fe5
ThanhNguyenPham-Steve <150330770+ThanhNguyenPham-Steve@users.noreply.github.com>
2025-06-20 09:02:06 +0000

    [automatic commit] 2025-06-20 09:02:06 UTC 203.59.201.100

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
