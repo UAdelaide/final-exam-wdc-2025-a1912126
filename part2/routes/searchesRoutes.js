68f10caba50773f415799967e7933525f874ab04
ThanhNguyenPham-Steve <150330770+ThanhNguyenPham-Steve@users.noreply.github.com>
2025-06-20 11:08:37 +0000

    [automatic commit] 2025-06-20 11:08:37 UTC 203.59.201.100

--- part2/routes/searchesRoutes.js
+++ part2/routes/searchesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET /api/dogs - fetch all dogs with owner info
+// GET /api/dogs - fetch all 
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
