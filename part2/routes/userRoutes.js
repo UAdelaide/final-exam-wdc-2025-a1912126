8085671dc0114a4dd058167b10bca7911e54cf0e
ThanhNguyenPham-Steve <150330770+ThanhNguyenPham-Steve@users.noreply.github.com>
2025-06-20 09:37:00 +0000

    [automatic commit] 2025-06-20 09:37:00 UTC 203.59.201.100

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -63,7 +63,7 @@ router.post('/login', async (req, res) => {
 
 // POST /api/users/logout
 router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
