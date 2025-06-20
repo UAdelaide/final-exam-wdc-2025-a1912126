1db474a9e296d8c792bd317cc70a262b96321721
ThanhNguyenPham-Steve <150330770+ThanhNguyenPham-Steve@users.noreply.github.com>
2025-06-20 05:23:34 +0000

    [automatic commit] 2025-06-20 05:23:34 UTC 203.59.201.100

--- part1/app.js
+++ part1/app.js
@@ -7,7 +7,7 @@ var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var searchesRouter = require('./routes/searches');
+var searchesRouter = require('./routes/searches.js');
 var app = express();
 
 // view engine setup
